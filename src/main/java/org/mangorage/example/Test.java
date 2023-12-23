c3ac7856836e6fd35d1e75fdfc696344a130931d
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-12-23 00:14:08 -0800

    Bumped to GradleUtils 3.0.2-beta2083

--- src/main/java/org/mangorage/example/Test.java
+++ src/main/java/org/mangorage/example/Test.java
@@ -1,6 +1,8 @@
 package org.mangorage.example;
 
-
+import com.google.gson.GsonBuilder;
+import joptsimple.OptionParser;
+import joptsimple.OptionSpec;
 import org.mangorage.mangobotapi.core.plugin.api.AddonPlugin;
 import org.mangorage.mangobotapi.core.plugin.impl.Plugin;
 
@@ -8,6 +10,11 @@ import org.mangorage.mangobotapi.core.plugin.impl.Plugin;
 public class Test extends AddonPlugin {
     public Test() {
         super("test");
-        System.out.println("LOADED ADDON!");
+        OptionParser optionParser = new OptionParser();
+        OptionSpec<String> VAL = optionParser.accepts("test").withRequiredArg().ofType(String.class);
+
+        var a = optionParser.parse(new String[]{"test", "129"});
+
+        System.out.println(a.valueOf(VAL));
     }
 }
