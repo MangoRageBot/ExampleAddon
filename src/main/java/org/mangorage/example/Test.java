aee1b09fc5e82e7260a4153d635b79be1cc59986
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-12-28 01:33:55 -0800

    Bumped GradleUtils

--- src/main/java/org/mangorage/example/Test.java
+++ src/main/java/org/mangorage/example/Test.java
@@ -1,5 +1,7 @@
 package org.mangorage.example;
 
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.kohsuke.github.GitHub;
 import org.mangorage.mangobotapi.core.plugin.api.AddonPlugin;
 import org.mangorage.mangobotapi.core.plugin.impl.Plugin;
 
@@ -12,5 +14,6 @@ public class Test extends AddonPlugin {
         super("test");
         //  Won't send if 'mangobot' isn't loaded...
         sendInterPluginMessage("mangobot", "getDate", () -> Date.from(Instant.EPOCH));
+        GitHubClient client = new GitHubClient();
     }
 }
