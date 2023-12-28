dace2da75826015ea06e1d7a0f06ee61426edb87
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-12-28 00:52:58 -0800

    Cleanup

--- src/main/java/org/mangorage/example/Test.java
+++ src/main/java/org/mangorage/example/Test.java
@@ -1,15 +1,10 @@
 package org.mangorage.example;
 
-import org.mangorage.mangobot.MangoBotPlugin;
-import org.mangorage.mangobotapi.core.plugin.PluginManager;
 import org.mangorage.mangobotapi.core.plugin.api.AddonPlugin;
-import org.mangorage.mangobotapi.core.plugin.api.CorePlugin;
 import org.mangorage.mangobotapi.core.plugin.impl.Plugin;
 
 import java.time.Instant;
 import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 
 @Plugin(id = "test", type = Plugin.Type.ADDON)
 public class Test extends AddonPlugin {
