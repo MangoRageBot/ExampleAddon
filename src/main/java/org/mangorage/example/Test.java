c54898f37ec15e140067be954316c4238d860239
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-12-28 00:52:38 -0800

    Bumped to GradleUtils 3.0.2-beta2103 and MangoBot 1.0.346 and mangobotplugin to 1.0.9 aswell as installer 2.2.0

--- src/main/java/org/mangorage/example/Test.java
+++ src/main/java/org/mangorage/example/Test.java
@@ -1,16 +1,21 @@
 package org.mangorage.example;
 
-import joptsimple.OptionParser;
+import org.mangorage.mangobot.MangoBotPlugin;
 import org.mangorage.mangobotapi.core.plugin.PluginManager;
 import org.mangorage.mangobotapi.core.plugin.api.AddonPlugin;
 import org.mangorage.mangobotapi.core.plugin.api.CorePlugin;
 import org.mangorage.mangobotapi.core.plugin.impl.Plugin;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Plugin(id = "test", type = Plugin.Type.ADDON)
 public class Test extends AddonPlugin {
     public Test() {
         super("test");
-        PluginManager.getPlugin("mangobot", CorePlugin.class).getCommandRegistry().addBasicCommand(new TestCommand());
-        OptionParser parser = new OptionParser();
+        //  Won't send if 'mangobot' isn't loaded...
+        sendInterPluginMessage("mangobot", "getDate", () -> Date.from(Instant.EPOCH));
     }
 }
--- src/main/java/org/mangorage/example/TestCommand.java
+++ src/main/java/org/mangorage/example/TestCommand.java
@@ -1,19 +1,30 @@
 package org.mangorage.example;
 
 import net.dv8tion.jda.api.entities.Message;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHMyself;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 import org.mangorage.mangobotapi.core.commands.Arguments;
 import org.mangorage.mangobotapi.core.commands.CommandResult;
 import org.mangorage.mangobotapi.core.commands.IBasicCommand;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class TestCommand implements IBasicCommand {
     @Override
     public CommandResult execute(Message message, Arguments arguments) {
-        message.reply("It worked! The addon works!!").queue();
+        message.reply("Addon Worked!").queue();
         return CommandResult.PASS;
     }
 
     @Override
     public String commandId() {
-        return "addonExample";
+        return "addonTest";
     }
 }
